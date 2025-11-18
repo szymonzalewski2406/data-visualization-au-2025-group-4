1c804212e969b8e6ee42f32c8992e24ca30e3919
Petros <p.tsitsekidis@gmail.com>
2025-11-18 20:30:31 +0100

    Fix folder doesn't exist bug

--- concat_all_data.py
+++ concat_all_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 champions = pd.read_csv('uefa_champions_data/uefa_gesamt.csv')
 conference = pd.read_csv('uefa_conference_data/uefa_gesamt.csv')
@@ -9,4 +10,6 @@ conference['league'] = 'Conference'
 europa['league'] = 'Europa'
 
 combined = pd.concat([champions, conference, europa])
+
+os.makedirs('combined_data', exist_ok=True)
 combined.to_csv('combined_data/uefa_all_leagues_combined.csv')
