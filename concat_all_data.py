f911723d54136275482ba4fe733eabe16490ef96
Petros <p.tsitsekidis@gmail.com>
2025-11-18 20:45:40 +0100

    Fix file locations

--- concat_all_data.py
+++ concat_all_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 
-champions = pd.read_csv('uefa_champions_data/uefa_gesamt.csv')
-conference = pd.read_csv('uefa_conference_data/uefa_gesamt.csv')
-europa = pd.read_csv('uefa_europa_data/uefa_gesamt.csv')
+champions = pd.read_csv('public/datasets/champions_league/champions_league_total.csv')
+conference = pd.read_csv('public/datasets/conference_league/conference_league_total.csv')
+europa = pd.read_csv('public/datasets/europa_league/europa_league_total.csv')
 
 champions['league'] = 'Champions'
 conference['league'] = 'Conference'
@@ -11,5 +11,5 @@ europa['league'] = 'Europa'
 
 combined = pd.concat([champions, conference, europa])
 
-os.makedirs('combined_data', exist_ok=True)
-combined.to_csv('combined_data/uefa_all_leagues_combined.csv')
+os.makedirs('public/datasets/uefa_combined', exist_ok=True)
+combined.to_csv('public/datasets/uefa_combined/uefa_all_leagues_combined.csv')
